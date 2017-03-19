b267ab9b9c2fbb3f6799f7bae2bec901b26b9074
Sven Lehnert <mail@sven-lehnert.de>
2017-03-19 14:19:37 +0100

    first working experimental version

--- loader.php
+++ loader.php
@@ -29,49 +29,92 @@
  ****************************************************************************
  */
 
+/*
+ * add  the lang attribute to the wp_dropdown_categories agrumenst array to maker sure only country specific terms get displayed
+ */
+add_filter( 'buddyforms_wp_dropdown_categories_args', 'buddyforms_polylang_wp_dropdown_categories_args', 10, 2 );
+function buddyforms_polylang_wp_dropdown_categories_args( $args, $post_id ){
+	global $buddyforms;
+		$args['lang'] = pll_get_post_language( $post_id, 'slug' );
+	return $args;
+}
+
+/*
+ * Display all languages in the post list?
+ */
 add_filter( 'buddyforms_user_posts_query_args', 'buddyforms_polylang_user_posts_query_args', 10, 1 );
-
-
 function buddyforms_polylang_user_posts_query_args( $query_args ){
+	global $buddyforms;
 
-	$query_args['lang'] = '';
+	if( isset( $query_args['form_slug']['polylang']['view_all_languages'] ) ){
+		$query_args['lang'] = '';
+	}
 
 	return $query_args;
 }
 
+/*
+ * Add a current language and the language switcher to the head of the form.
+ */
 add_filter( 'buddyforms_form_hero_top', 'buddyforms_polylang_form_hero_top', 10, 2 );
-
 function buddyforms_polylang_form_hero_top( $form_html, $form_slug ){
-
 	global $post_id;
-
-	echo $post_id . '<br>';
-
-	$languages = pll_languages_list($post_id);
-	echo 'All Registered Languages<br>';
-	echo '<pre>';
-	print_r($languages);
-	echo '</pre>';
-	echo 'current language: ' . pll_get_post_language( $post_id, 'slug' );
-
 
 	$translations = pll_the_languages(array('raw'=>1));
 
-	echo '<br>Translations<br>';
-	foreach($languages as $language){
+	$tmp = __('Post Language: ', 'buddyforms' ) . '<img src="' . $translations[pll_get_post_language( $post_id, 'slug' )]['flag'] . '">';
 
-		echo $language . ' - ' . pll_get_post($post_id, $language) . ' <img src="' . $translations[$language]['flag'] . '"> <br>';
+	$languages = pll_languages_list($post_id);
+	$tmp .= __(' Edit different language', 'buddyforms');
+		foreach($languages as $language){
 
+			if($language != pll_get_post_language( $post_id, 'slug' ) ){
+				$tmp .= ' <img src="' . $translations[$language]['flag'] . '""> ' .apply_filters( 'buddyforms_loop_edit_post_link', buddyforms_get_edit_post_link( pll_get_post($post_id, $language) ), pll_get_post($post_id, $language) );
+			}
+		}
+
+	return $form_html . $tmp;
+}
+
+
+/*
+ * Add the country flag to the posts list
+ */
+add_action( 'buddyforms_the_loop_item_last', 'buddyforms_polylang_the_loop_item_last' );
+function buddyforms_polylang_the_loop_item_last( $post_id ){
+	$translations = pll_the_languages(array('raw'=>1));
+	echo 'Language: <img src="' . $translations[pll_get_post_language( $post_id, 'slug' )]['flag'] . '">';
+}
+
+/*
+ * Make the Form Labels Translatable
+ */
+add_action('admin_init', 'register_form_fields');
+function register_form_fields(){
+	global $buddyforms;
+
+	if( is_array( $buddyforms ) ){
+		foreach ( $buddyforms as $form_slug => $buddyform ){
+			if( isset($buddyform['form_fields']) && is_array($buddyform['form_fields'])){
+				foreach ( $buddyform['form_fields'] as $field ){
+					pll_register_string( 'buddyforms', $field['name'], $form_slug );
+					pll_register_string( 'buddyforms', $field['description'], $form_slug );
+				}
+			}
+
+		}
 	}
-
-	echo pll_translate_string('Svenson', 'en');
-
-	return $form_html;
 
 }
 
-add_action('admin_init', 'register_form_fields');
-
-function register_form_fields(){
-	pll_register_string('buddyforms', 'Firmenname');
+/*
+ * Translate Form Fields Name and description
+ */
+add_filter( 'buddyforms_form_field_name', 'buddyforms_polylang_form_field_name', 10, 2 );
+function buddyforms_polylang_form_field_name( $name, $post_id ){
+	return pll_translate_string($name, pll_get_post_language( $post_id, 'slug' ));
+}
+add_filter( 'buddyforms_form_field_description', 'buddyforms_polylang_form_field_description', 10, 2 );
+function buddyforms_polylang_form_field_description( $description, $post_id ){
+	return pll_translate_string($description, pll_get_post_language( $post_id, 'slug' ));
 }
